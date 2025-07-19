291494051925004647422507a1bd6f2d71770a07
Nuvisa09 <nurnisa0922@gmail.com>
2025-07-19 20:28:09 +0700

    first commit

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full bg-slate-800 p-4 text-white text-center mt-auto ">
+      <p>© {new Date().getFullYear()} Nor Vita Sari. All rights reserved.</p>
+    </footer>
+  );
+}
